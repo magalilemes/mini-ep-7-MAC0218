ede866aa3ebebb68e2114bb9e697a5699ccf6fb0
Magali Lemes <magalilemes00@gmail.com>
2022-05-13 14:34:20 -0300

    complete AnalisadorDeAprovacao class

--- src/main/kotlin/domain/AnalisadorDeAprovacao.kt
+++ src/main/kotlin/domain/AnalisadorDeAprovacao.kt
@@ -1,15 +1,20 @@
 package domain
 
+import domain.criterios.CriterioDeAprovacao
+
 class AnalisadorDeAprovacao {
 
-    // ---------------------------------
-    //
-    //      Seu código deve vir aqui
-    //
-    //      Defina atributos e métodos conforme especificado
-    //      no arquivo de teste encontrado em
-    //      'src/test/kotlin/domain/AnalisadorDeAprovacaoTest'
-    //
-    // ---------------------------------
+	private lateinit var criterioDeAprovacao: CriterioDeAprovacao
 
+	fun defineCriterio(criterio: CriterioDeAprovacao){
+		criterioDeAprovacao = criterio
+	}
+
+	fun fechaBoletim(boletim: Boletim): BoletimFechado{
+
+		return BoletimFechado(boletim.mediaEPs,
+			boletim.mediaMiniEPs,
+			criterioDeAprovacao.mediaFinal(boletim),
+			criterioDeAprovacao.estaAprovado(boletim))
+	}
 }
